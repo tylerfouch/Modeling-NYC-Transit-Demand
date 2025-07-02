6d2a0e826684a59ddbdc6cea82a6c0e402695e8f
Tyler Fouch <fouchtyler@gmail.com>
2025-07-02 13:45:43 -0400

    Add plot and predict

---
+++ plot.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import statsmodels.api as sm
+
+df = pd.read_csv("csv_database/df.csv")
+
+# Fit model
+X = df[
+    ["LOG_B_JOB_DENSITY", "DISTANCE", "TERMINUS", "TOTAL_FREQ", "ADA",
+     "COMMUTER", "FREQ_X_COMMUTER", "B_DENSITY"]
+    ]
+X = sm.add_constant(X)
+y = df["LOG_2023"]
+model = sm.OLS(y, X).fit()
+
+# Add columns for predicted vs. actual values
+df["ACTUAL"] = np.expm1(df["LOG_2023"])
+df["PREDICTED"] = np.expm1(model.predict(X))
+
+# Linear plot
+plt.figure(figsize=(10, 7))
+sns.set(style="whitegrid")
+sns.scatterplot(df, x = "ACTUAL", y = "PREDICTED")
+sns.regplot(df, x = "ACTUAL", y = "PREDICTED", scatter = False)
+tick_vals = list(range(0, 60_000_001, 5_000_000))
+tick_labels = [f"{x//1_000_000}M" for x in tick_vals]
+plt.xticks(tick_vals, tick_labels)
+plt.yticks(tick_vals, tick_labels)
+plt.xlabel("Actual Ridership (millions/year)")
+plt.ylabel("Predicted Ridership (millions/year)")
+plt.title("Predicted vs. Actual Ridership (2023, linear scale)")
+plt.grid(True, which = "both", linestyle= "--")
+plt.tight_layout()
+plt.savefig("plots/linear_plot.png")
+
+# Log plot
+plt.figure(figsize=(10, 7))
+sns.set(style="whitegrid")
+sns.scatterplot(df, x = "ACTUAL", y = "PREDICTED")
+sns.regplot(df, x = "ACTUAL", y = "PREDICTED", scatter = False)
+plt.xscale("log")
+plt.yscale("log")
+ticks = [1e4, 1e5, 1e6, 1e7, 6e7]
+labels = ["10K", "100K", "1M", "10M", "60M"]
+plt.xticks(ticks, labels)
+plt.yticks(ticks, labels)
+plt.xlabel("Actual Ridership (millions/year)")
+plt.ylabel("Predicted Ridership (millions/year)")
+plt.title("Predicted vs. Actual Ridership (2023, log scale)")
+plt.grid(True, which = "both", linestyle= "--")
+plt.tight_layout()
+plt.savefig("plots/log_plot.png")
---
+++ predict.py
@@ -0,0 +1,87 @@
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+from shapely import distance
+from shapely import wkt
+from shapely.geometry import Point
+from sklearn.preprocessing import StandardScaler
+
+# Import datasets
+data = pd.read_csv("csv_database/data.csv")
+merge = pd.read_csv("csv_database/merge.csv")
+merge_t = pd.read_csv("csv_database/merge_t.csv")
+jobs = pd.read_csv("csv_database/jobs.csv")
+jobs_t = pd.read_csv("csv_database/jobs_t.csv")
+frequencies = pd.read_csv("csv_database/frequencies.csv")
+merge["geometry"] = merge["geometry"].apply(wkt.loads)
+merge = gpd.GeoDataFrame(merge, geometry="geometry")
+
+# Standardize population density
+merge["DENSITY"] = StandardScaler().fit_transform(merge[["DENSITY"]])
+
+# Function to predict ridership for a point with given parameters
+def predict_ridership(latitude, longitude, frequency, terminus,
+                      commuter, transfer):
+    coord = Point(longitude, latitude) # Create shapely Point
+
+    # Find if Point is within a block group in the dataset
+    for i, row in merge.iterrows():
+        polygon = row["geometry"]
+        if polygon.contains(coord):
+            index_id = i
+            geo_id = row["BLOCK GROUP"]
+            geo_id_t = row["GEOID_T"]
+            break
+
+    # Data preparation
+    P_B_JOB_DENSITY = jobs.loc[
+        jobs[
+            "BLOCK GROUP"] == geo_id,
+        "JOBS"].values[0] / merge.at[index_id, "AREA"]
+    P_B_DENSITY = merge.at[index_id, "DENSITY"]
+    P_DISTANCE = distance(coord, Point((-73.9840, 40.7549)))
+    LOG_FREQ_X_COMMUTER = np.log1p(frequency) * commuter
+
+    # Equation
+    log_pred = (
+            11.7557
+            + 0.1002 * np.log1p(P_B_JOB_DENSITY)
+            - 6.8646 * np.log1p(P_DISTANCE)
+            + 0.4503 * terminus
+            + 0.8109 * np.log1p(frequency)
+            + 0.2575 * 1
+            + 4.2903 * commuter
+            - 0.9410 * LOG_FREQ_X_COMMUTER
+            + 0.0818 * P_B_DENSITY
+        )
+    return np.exp(log_pred)
+
+# Function to predict ridership for multiple given stations
+def predict_ridership_for_data_frame(test_data, freq):
+    given_data = pd.read_csv(test_data)
+    results = pd.DataFrame({"Station": [], "Ridership": []})
+    for i, row in given_data.iterrows():
+        routes = row["Lines"].split(",")
+        given_data.at[i, "Lines"] = routes
+        total_freq = 0
+        for j in routes:
+            for k, row_k in freq.iterrows():
+                if row_k["Service"] == j:
+                    total_freq = total_freq + row_k["TPH"]
+                given_data.at[i, "Frequency"] = total_freq
+    return pd.concat(
+                [results,
+                 pd.DataFrame(
+                     {"Station": [row["Stop Name"]],
+                      "Ridership": [predict_ridership
+                      (row["Latitude"], row["Longitude"],
+                       total_freq, row["Terminus"],
+                       row["Commuter"], row["Transfer"])]
+                      })])
+
+# Predict ridership for stations in "TEST.csv"
+save_ridership = predict_ridership_for_data_frame(
+    "test_data/TEST.csv", frequencies)
+
+# Export calculated ridership to CSV
+save_ridership.to_csv("test_data/calculated_ridership.csv")
